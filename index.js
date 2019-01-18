39c60e00fcfc3d72fd533a87b96d3e5297b37783
heeman74 <heeman0918@gmail.com>
2019-01-18 10:43:16 -0800

    added controllers routes, views folder.

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
-
+const path = require('path')
 const app = express();
+const bodyParser = require('body-parser')
 
+const yelpRoutes = require('./routes/yelp')
+
+
+
+app.use(bodyParser.urlencoded({ extended: false }))
 
 app.listen(3000, 
   console.log('Server is listening port 3000'))
