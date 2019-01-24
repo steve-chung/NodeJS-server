ed1e23cb8499aae78a05d274bb0c7e9e2c44030f
heeman74 <heeman0918@gmail.com>
2019-01-23 20:59:13 -0800

    connected to postgres using sequalize

--- index.js
+++ index.js
@@ -3,10 +3,12 @@ const express = require('express')
 const path = require('path')
 const app = express();
 const bodyParser = require('body-parser')
-const yelpRoutes = require('./routes/yelp')
+const apiRoutes = require('./routes/api')
+const user = require('./models/user')
+
 
 app.use(bodyParser.urlencoded({ extended: false }))
-app.use(yelpRoutes)
+app.use('/api', apiRoutes)
 
 app.listen(process.env.PORT, () => {
   console.log('Listening on port', process.env.PORT)
