1d2b47ab783c5bd818aba6580a1e2829cb0fe1b4
heeman74 <heeman0918@gmail.com>
2019-01-27 00:07:12 -0800

    user login and respond with tokens.  create login controller

--- controllers/auth.js
+++ controllers/auth.js
@@ -2,6 +2,27 @@ const User = require('../models/user')
 const { createTokens } = require('../util/token')
 const bcrypt = require('bcryptjs')
 
+
+const loginRes = (user, res) => {
+  let accessToken
+  let refreshToken
+  createTokens(user, '123', '654')
+    .then((tokens) => {
+      accessToken = tokens[0]
+      refreshToken = tokens[1]
+      return res.status(200).json({
+        message:`User ${user.name} was created`,
+        id: user.id,
+        username: user.name,
+        accessToken,
+        refreshToken
+      })
+    })
+    .catch(err => {
+      console.log(err)
+    })
+}
+
 exports.register = (req, res, next) => {
   const email = req.body.email
   const name = req.body.name
@@ -16,21 +37,7 @@ exports.register = (req, res, next) => {
         .spread((user, created) => {
           
           if (created) {
-            let accessToken
-            let refreshToken
-            createTokens(user, '123', '654')
-              .then((tokens) => {
-                accessToken = tokens[0]
-                refreshToken = tokens[1]
-                return res.status(200).json({
-                  message:`User ${user.name} was created`,
-                  id: user.id,
-                  username: user.name,
-                  accessToken,
-                  refreshToken
-                })
-              })
-              return 
+              return loginRes(user, res)
             } else {
               return res.status(400).json({
                 message:`User ${name} is already exist, please use different name`
@@ -48,8 +55,40 @@ exports.register = (req, res, next) => {
 }
 
 exports.login = (req, res, next) => {
-
+  const email = req.body.email
+  console.log(email)
+  const password = req.body.password
+  User.findOne({ where: { email }, raw: true })
+    .then (user => {
+      console.log(user)
+      if (!user) {
+        // user with provided email not found
+        throw new Error('Invalid login');
+      }
+      console.log(user.password)
+      bcrypt.compare(password, user.password)
+        .then((valid) => {
+          if (!valid) {
+            throw new Error('Invalid login');
+          }else {
+            loginRes(user, res)
+          }
+        })
+        .catch(err => {
+          console.log(err)
+          return res.status(400).json({
+            message: 'Invalid login'
+          })
+        })
+    })
+    .catch(err => {
+      console.log(err)
+      res.status(500).json({
+        message:'Something went wrong'
+      })
+    })
 }
+
 
 exports.logout = (req, res, next) => {
 
